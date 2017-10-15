54d05dcd02d9d5a5c42edd3aac4f47d63cd371f3
alex <alex@staticlibs.net>
2017-10-16 00:54:00 +0100

    verbose logging

--- src/wilton_db.cpp
+++ src/wilton_db.cpp
@@ -17,6 +17,14 @@
 
 #include "wilton/support/alloc_copy.hpp"
 #include "wilton/support/buffer.hpp"
+#include "wilton/support/handle_registry.hpp"
+#include "wilton/support/logging.hpp"
+
+namespace { // anonymous
+
+const std::string LOGGER = std::string("wilton.DBConnection");
+
+} // namespace
 
 struct wilton_DBConnection {
 private:
@@ -56,8 +64,10 @@ char* wilton_DBConnection_open(
         uint16_t conn_url_len_u16 = static_cast<uint16_t> (conn_url_len);
         std::string conn_url_str{conn_url, conn_url_len_u16};
         sl::orm::connection conn{conn_url_str};
+        wilton::support::log_debug(LOGGER, "Creating connection, URL: [" + conn_url_str + "] ...");
         wilton_DBConnection* conn_ptr = new wilton_DBConnection{std::move(conn)};
         *conn_out = conn_ptr;
+        wilton::support::log_debug(LOGGER, "Connection created, handle: [" + wilton::support::strhandle(conn_ptr) + "]");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -85,11 +95,14 @@ char* wilton_DBConnection_query(
         uint32_t sql_text_len_u32 = static_cast<uint32_t> (sql_text_len);
         std::string sql_text_str{sql_text, sql_text_len_u32};
         auto json = params_json_len > 0 ? sl::json::load({params_json, params_json_len}) : sl::json::value();
+        wilton::support::log_debug(LOGGER, "Executing DQL, SQL: [" + sql_text_str + "]," +
+                " parameters: [" + json.dumps() + "], handle: [" + wilton::support::strhandle(conn) + "] ...");
         std::vector<sl::json::value> rs = conn->impl().query(sql_text_str, json);
         auto rs_json = sl::json::value(std::move(rs));
         auto span = wilton::support::make_json_buffer(rs_json);
         *result_set_out = span.value().data();
         *result_set_len_out = static_cast<int>(span.value().size());
+        wilton::support::log_debug(LOGGER, "Execution complete, result: [" + rs_json.dumps() + "]");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -113,7 +126,10 @@ char* wilton_DBConnection_execute(
         uint32_t sql_text_len_u32 = static_cast<uint32_t> (sql_text_len);
         std::string sql_text_str{sql_text, sql_text_len_u32};
         auto json = params_json_len > 0 ? sl::json::load({params_json, params_json_len}) : sl::json::value();
+        wilton::support::log_debug(LOGGER, "Executing DML, SQL: [" + sql_text_str + "]," +
+                " parameters: [" + json.dumps() + "], handle: [" + wilton::support::strhandle(conn) + "] ...");
         conn->impl().execute(sql_text_str, json);
+        wilton::support::log_debug(LOGGER, "Execution complete");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -124,7 +140,9 @@ char* wilton_DBConnection_close(
         wilton_DBConnection* conn) {
     if (nullptr == conn) return wilton::support::alloc_copy(TRACEMSG("Null 'conn' parameter specified"));
     try {
+        wilton::support::log_debug(LOGGER, "Closing connection, handle: [" + wilton::support::strhandle(conn) + "] ...");
         delete conn;
+        wilton::support::log_debug(LOGGER, "Connection closed");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -137,9 +155,11 @@ char* wilton_DBTransaction_start(
     if (nullptr == conn) return wilton::support::alloc_copy(TRACEMSG("Null 'conn' parameter specified"));
     if (nullptr == tran_out) return wilton::support::alloc_copy(TRACEMSG("Null 'tran_out' parameter specified"));
     try {
+        wilton::support::log_debug(LOGGER, "Starting transaction, connection handle: [" + wilton::support::strhandle(conn) + "] ...");
         sl::orm::transaction tran = conn->impl().start_transaction();
         wilton_DBTransaction* tran_ptr = new wilton_DBTransaction(std::move(tran));
         *tran_out = tran_ptr;
+        wilton::support::log_debug(LOGGER, "Transaction started, handle: [" + wilton::support::strhandle(tran_ptr) + "]");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -150,8 +170,10 @@ char* wilton_DBTransaction_commit(
         wilton_DBTransaction* tran) {
     if (nullptr == tran) return wilton::support::alloc_copy(TRACEMSG("Null 'tran' parameter specified"));
     try {
+        wilton::support::log_debug(LOGGER, "Committing transaction, handle: [" + wilton::support::strhandle(tran) + "] ...");
         tran->impl().commit();
         delete tran;
+        wilton::support::log_debug(LOGGER, "Transaction committed");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
@@ -162,7 +184,9 @@ char* wilton_DBTransaction_rollback(
         wilton_DBTransaction* tran) {
     if (nullptr == tran) return wilton::support::alloc_copy(TRACEMSG("Null 'tran' parameter specified"));
     try {
+        wilton::support::log_debug(LOGGER, "Rolling back transaction, handle: [" + wilton::support::strhandle(tran) + "] ...");
         delete tran;
+        wilton::support::log_debug(LOGGER, "Transaction rolled back");
         return nullptr;
     } catch (const std::exception& e) {
         return wilton::support::alloc_copy(TRACEMSG(e.what() + "\nException raised"));
